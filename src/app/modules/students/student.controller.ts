c866d8ad1706afdddcd82bd2000ef0b91954993c
Mostak Ahamed Nishat <mostakahamed484@gmail.com>
2025-01-24 01:10:52 +0600

    Custom asyncHandler added

--- src/app/modules/students/student.controller.ts
+++ src/app/modules/students/student.controller.ts
@@ -2,50 +2,42 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { StudentServices } from "./student.service";
 import { studentValidationSchema } from "./student.validation";
 import { z } from "zod";
+import catchAsyncHandler from "../../utils/asyncHandler";
+
 // Create Student Controller
 
-const getAllStudents: RequestHandler = async (req, res, next) => {
-  try {
-    const response = await StudentServices.getAllStudentsFromDB();
-    return res.status(200).json({
-      success: true,
-      message: "Successfully get all the students",
-      data: response,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const getAllStudents = catchAsyncHandler(async (req, res) => {
+  const response = await StudentServices.getAllStudentsFromDB();
+  return res.status(200).json({
+    success: true,
+    message: "Successfully get all the students",
+    data: response,
+  });
+});
 
-const getSingleStudentById: RequestHandler = async (req, res, next) => {
-  try {
+const getSingleStudentById: RequestHandler = catchAsyncHandler(
+  async (req, res) => {
     const { studentId } = req.params;
     const response = await StudentServices.getSingleStudentByIdFromDB(
       studentId
     );
-    return res.status(200).json({
+    res.status(200).json({
       success: true,
       message: "Student found successfully",
       data: response,
     });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
-const deleteStudentId: RequestHandler = async (req, res, next) => {
-  try {
-    const { studentId } = req.params;
-    const response = await StudentServices.deleteStudentByIdFromDB(studentId);
-    return res.status(200).json({
-      success: true,
-      message: "Student delete successfully",
-      data: response,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const deleteStudentId: RequestHandler = catchAsyncHandler(async (req, res) => {
+  const { studentId } = req.params;
+  const response = await StudentServices.deleteStudentByIdFromDB(studentId);
+  res.status(200).json({
+    success: true,
+    message: "Student delete successfully",
+    data: response,
+  });
+});
 
 export const StudentController = {
   getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -10,9 +10,10 @@ import { studentValidationSchema } from "../students/student.validation";
 import config from "../../config";
 import sendResponse from "../../utils/sendResponse";
 import status from "http-status";
+import catchAsyncHandler from "../../utils/asyncHandler";
 
-const createStudent: RequestHandler = async (req, res, next) => {
-  try {
+const createStudent: RequestHandler = catchAsyncHandler(
+  async (req, res, next) => {
     const { password, student: studentData } = req.body;
 
     //Will call service function to send this data
@@ -20,7 +21,6 @@ const createStudent: RequestHandler = async (req, res, next) => {
       password,
       studentData
     );
-
     response &&
       sendResponse(res, {
         statusCode: status.OK,
@@ -28,10 +28,8 @@ const createStudent: RequestHandler = async (req, res, next) => {
         message: "Student is created successfully",
         data: response,
       });
-  } catch (error) {
-    next(error);
   }
-};
+);
 
 export const UserControllers = {
   createStudent,
